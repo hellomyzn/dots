cc7533631a7bca19a31df93f5585262ad87a82de
hellomyzn <em711th@icloud.com>
2023-01-28 15:48:36 +1100

    spacelauncher

---
+++ .config/spacelauncher/README.md
@@ -0,0 +1,9 @@
+<space> + i : iTerm
+<space> + g : Google Chrome
+<space> + b : Brave
+<space> + f : Firefox
+<space> + n : Notes
+<space> + s : System
+<space> + m : Music
+<space> + d : Docker
+<space> + @ : mail
--- bin/setup.sh
+++ bin/setup.sh
@@ -20,7 +20,7 @@ TO=$HOME
 FILE=".tmux.conf"
 copy_file $FROM $TO $FILE
 
-
+# tmux sessions
 PATH_TMUX_SESSIONS=".config/tmux/sessions"
 TO=$HOME/$PATH_TMUX_SESSIONS
 FROM=$PWD/$PATH_TMUX_SESSIONS
@@ -30,10 +30,7 @@ for f in $SESSION_FILES; do
     copy_file $FROM $TO $f
     ln -snfv $FROM/$f $TO/$f
 done
-# echo $array
-# for eachValue in ${array[@]}; do
-#     echo ${eachValue}
-# done
+
 # for f in $FROM/*; do
 #     [ "$f" = ".git" ] && continue
 #     [ "$f" = ".gitconfig.local.template" ] && continue
@@ -43,28 +40,3 @@ done
 #     # ln -snfv $PWD/$f $TO/$f
     
 # done
-
-
-
-# # tmux sessions
-# mkdir -p $HOME/.config/tmux
-# FROM=".config/tmux/sessions"
-# TO="${HOME}/.config/tmux/sessions"
-# FILE=".tmux.conf"
-# PATH_SESSIONS=".config/tmux/sessions"
-# if [ -d $HOME/$PATH_SESSIONS ]; then
-#     echo -e "${WARNING} : $PATH_SESSIONS exists"
-#     ls $PATH_SESSIONS/* 
-#     read -p "Do you want to update the above files? [y/n]: " yn
-#     if [ $yn = "y" ]; then 
-#         ls $PATH_SESSIONS/* | xargs -I{} cp {} $HOME/{}
-#         echo -e "${SUCCESS} : Copied: ${HOME}/${PATH_SESSIONS}\n"
-#     else
-#         echo -e "${CANCEL} : Canceled:  ${HOME}/${PATH_SESSIONS}\n"
-#     fi
-# else
-#     # copy from sessions dir to $HOME/.config/tmux/sessions
-#     mkdir -p $HOME/$PATH_SESSIONS
-#     ls $PATH_SESSIONS/* | xargs -I{} cp {} $HOME/{}
-#     echo -e "${SUCCESS} : Copied: ${HOME}/${PATH_SESSIONS}\n"
-# fi
