8b3a926aad1ee2c31b0241fcefc16195e515588f
hellomyzn <em711th@icloud.com>
2023-01-30 18:24:08 +1100

    update : setup

--- bin/copy.sh
+++ bin/copy.sh
@@ -1,32 +1,35 @@
+
 # $1: path copy from
 # $2: path copy to
+# $3: space to adjust to show
 function copy_file(){
+    SPACE=$3
     # is symbolic link exists? if it's no, ask you want to overwrite?
     if [ -L $2 ] ; then
-        echo  "\n${WARNING} : ${2} exists as a symbolic link (${2})"
-        read -p "Do you want to update ${2} ? [y/n]: " yn
+        echo  "\n${WARNING}${2} exists as a symbolic link (${2})"
+        read -p "${SPACE}Do you want to update ${2} ? [y/n]: " yn
         if [ $yn = "y" ]; then 
             ln -snfv $1 $2
-            echo  "${SUCCESS} : Copied: ${2}\n"
+            echo  "${SUCCESS}Copied: ${2}\n"
             return
         else
-            echo  "${CANCEL} : Canceled: ${2}\n"
+            echo  "${CANCEL}Canceled: ${2}\n"
             return
         fi
     fi
 
     # is file exists? if it's no, ask you want to overwrite?
     if [ -f $2 ] ; then
-        echo  "\n${WARNING} : ${2} exists as a file (${2})"
-        read -p "        Do you want to update ${2} ? [y/n]: " yn
+        echo  "\n${WARNING}${2} exists as a file (${2})"
+        read -p "${SPACE}Do you want to update ${2} ? [y/n]: " yn
         if [ $yn = "y" ]; then 
             rm $2
-            echo  "${INFO} : Removed: ${2}"
+            echo  "${INFO}Removed: ${2}"
             ln -snfv $1 $2
-            echo  "${SUCCESS} : Copied: ${2}\n"
+            echo  "${SUCCESS}Copied: ${2}\n"
             return
         else
-            echo  "${CANCEL} : Canceled: ${2}\n"
+            echo  "${CANCEL}Canceled: ${2}\n"
             return
         fi
     fi
@@ -38,22 +41,24 @@ function copy_file(){
 
 # $1: path copy from
 # $2: path copy to
+# $3: space to adjust to show
 function copy_dir(){
+    SPACE=$3
     # is symbolic link exists? if it's no, ask you want to overwrite?
     if [ -d $2 ] ; then
-        echo  "\n${WARNING} : ${1} exists as a symbolic link"
-        read -p "Do you want to update ${2} ? [y/n]: " yn
+        echo  "\n${WARNING}${1} exists as a symbolic link"
+        read -p "${SPACE}Do you want to update ${2} ? [y/n]: " yn
         if [ $yn = "y" ]; then 
             ln -snfv $1 $2
-            echo  "${SUCCESS} : Copied: ${2}\n"
+            echo  "${SUCCESS}Copied: ${2}\n"
             return
         else
-            echo  "${CANCEL} : Canceled: ${2}\n"
+            echo  "${CANCEL}Canceled: ${2}\n"
             return
         fi
     fi
     
     ln -snfv $1 $2
-    echo  "${SUCCESS} : Copied: ${2}\n"
+    echo  "${SUCCESS}Copied: ${2}\n"
     
 }
