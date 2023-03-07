2f5852c36886bc824e37d66cbf294354aca3c769
hellomyzn <em711th@icloud.com>
2023-03-08 08:44:58 +0900

    update(plugin.lua): add option for gitsigns

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -246,6 +246,8 @@ use({
         -- vim.keymap.set('n', '[h', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true, buffer = bufnr })
         vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
         vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
+        vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
+        vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
         vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
         vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
       end,
