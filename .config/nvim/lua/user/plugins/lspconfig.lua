7d743bfbe0808d504049c4b55df416dd8de7fa5f
hellomyzn <em711th@icloud.com>
2023-03-15 09:51:01 +0900

    add: language server

--- .config/nvim/lua/user/plugins/lspconfig.lua
+++ .config/nvim/lua/user/plugins/lspconfig.lua
@@ -24,6 +24,30 @@ vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
 vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
 vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>')
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+-- TODO: this is not working
+vim.keymap.set('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+
+-- Diagnostic configuration
+vim.diagnostic.config({
+  virtual_text = false,
+  severity_sort = true,
+  float = {
+    source = true,
+    focus = false,
+    format = function(diagnostic)
+      if diagnostic.user_data ~= nil and diagnostic.user_data.lsp.code ~= nil then
+        return string.format('%s: %s', diagnostic.user_data.lsp.code, diagnostic.message)
+      end
+      return diagnostic.message
+    end,
+  },
+})
+
+-- Sign configuration
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '􀇿', texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+
 
 
