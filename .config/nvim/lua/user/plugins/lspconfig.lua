2fc1878ca342c87864eb4a0ccce8bf7195f69914
hellomyzn <em711th@icloud.com>
2023-03-28 12:03:07 +0900

    fix (lspconfig): null-ls bug

--- .config/nvim/lua/user/plugins/lspconfig.lua
+++ .config/nvim/lua/user/plugins/lspconfig.lua
@@ -1,6 +1,6 @@
 -- Setup Mason to automatically install LSP servers
--- require('mason').setup()
--- require('mason-lspconfig').setup({ automatic_installation = true })
+require('mason').setup()
+require('mason-lspconfig').setup({ automatic_installation = true })
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -54,7 +54,7 @@ require('lspconfig').volar.setup({
 })
 
 -- Tailwind CSS
-require('lspconfig').tailwindcss.setup({ 
+require('lspconfig').tailwindcss.setup({
   capabilities = capabilities,
 })
 
