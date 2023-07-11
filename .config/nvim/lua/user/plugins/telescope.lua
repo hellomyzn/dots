0541c102be4bddf27ec53ee0752fd5638f459b8e
hellomyzn <em711th@icloud.com>
2023-07-12 08:30:48 +0900

    refactor(telescope): keymaps and code style

--- .config/nvim/lua/user/plugins/telescope.lua
+++ .config/nvim/lua/user/plugins/telescope.lua
@@ -1,5 +1,5 @@
-local telescope = require('telescope')
-local actions = require('telescope.actions')
+local telescope = require("telescope")
+local actions = require("telescope.actions")
 
 vim.cmd([[
   highlight link TelescopePromptTitle PMenuSel
@@ -11,48 +11,52 @@ vim.cmd([[
 ]])
 
 telescope.setup({
-  defaults = {
-    path_display = { truncate = 1 },
-    prompt_prefix = '   ',
-    selection_caret = '  ',
-    layout_config = {
-      prompt_position = 'top',
-    },
-    sorting_strategy = 'ascending',
-    mappings = {
-      i = {
-        ['<esc>'] = actions.close,
-        ['<C-Down>'] = actions.cycle_history_next,
-        ['<C-Up>'] = actions.cycle_history_prev,
-      },
-    },
-    file_ignore_patterns = { '.git/' },
-  },
-  pickers = {
-    find_files = {
-      hidden = true,
-    },
-    buffers = {
-      previewer = false,
-      layout_config = {
-        width = 80,
-      },
-    },
-    oldfiles = {
-      prompt_title = 'History',
-    },
-    lsp_references = {
-      previewer = false,
-    },
-  },
+	defaults = {
+		path_display = { truncate = 1 },
+		prompt_prefix = "   ",
+		selection_caret = "  ",
+		layout_config = {
+			prompt_position = "top",
+		},
+		sorting_strategy = "ascending",
+		mappings = {
+			i = {
+				["<esc>"] = actions.close,
+				["<C-Down>"] = actions.cycle_history_next,
+				["<C-Up>"] = actions.cycle_history_prev,
+			},
+		},
+		file_ignore_patterns = { ".git/" },
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+		buffers = {
+			previewer = false,
+			layout_config = {
+				width = 80,
+			},
+		},
+		oldfiles = {
+			prompt_title = "History",
+		},
+		lsp_references = {
+			previewer = false,
+		},
+	},
 })
 
-require('telescope').load_extension('fzf')
-require('telescope').load_extension('live_grep_args')
+require("telescope").load_extension("fzf")
+require("telescope").load_extension("live_grep_args")
 
-vim.keymap.set('n', '<leader>f', [[<cmd>lua require('telescope.builtin').find_files()<CR>]])
-vim.keymap.set('n', '<leader>F', [[<cmd>lua require('telescope.builtin').find_files({ no_ignore = true, prompt_title = 'All Files' })<CR>]]) -- luacheck: no max line length
-vim.keymap.set('n', '<leader>b', [[<cmd>lua require('telescope.builtin').buffers()<CR>]])
-vim.keymap.set('n', '<leader>g', [[<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>]])
-vim.keymap.set('n', '<leader>h', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]])
-vim.keymap.set('n', '<leader>s', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]])
+vim.keymap.set("n", "<leader>f", [[<cmd>lua require('telescope.builtin').find_files()<CR>]])
+vim.keymap.set(
+	"n",
+	"<leader>F",
+	[[<cmd>lua require('telescope.builtin').find_files({ no_ignore = true, prompt_title = 'All Files' })<CR>]]
+) 
+vim.keymap.set("n", "<leader>fb", [[<cmd>lua require('telescope.builtin').buffers()<CR>]])
+vim.keymap.set("n", "<leader>fg", [[<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>]])
+vim.keymap.set("n", "<leader>fh", [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]])
+vim.keymap.set("n", "<leader>fs", [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]])
