3c8d012966b36087ef7672655407794f57e9c8fa
hellomyzn <em711th@icloud.com>
2026-03-23 19:23:01 +0900

    update: git-repo

--- src/git-repo.sh
+++ src/git-repo.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-mkdir -p ~/projects
-cd projects
-
-git clone git@github.com:hellomyzn/footprints.git 
-git clone git@github.com:hellomyzn/yt-notifier.git
-git clone git@github.com:hellomyzn/TIL.git 
-git clone git@github.com:hellomyzn/myzn.git 
-git clone git@github.com:hellomyzn/yt-analysis.git
-git clone git@github.com:hellomyzn/polydock.git
-git clone git@github.com:hellomyzn/nf-analysis.git
-git clone git@github.com:hellomyzn/kindle2jpeg.git
-git clone git@github.com:hellomyzn/read_later.git 
-
-cd projects/dots
+git clone git@github.com:hellomyzn/footprints.git ~/projects/footprints
+git clone git@github.com:hellomyzn/yt-notifier.git ~/projects/yt-notifier
+git clone git@github.com:hellomyzn/TIL.git ~/projects/TIL
+git clone git@github.com:hellomyzn/myzn.git ~/projects/myzn
+git clone git@github.com:hellomyzn/yt-analysis.git ~/projects/yt-analysis
+git clone git@github.com:hellomyzn/polydock.git ~/projects/polydock
+git clone git@github.com:hellomyzn/nf-analysis.git ~/projects/nf-analysis
+git clone git@github.com:hellomyzn/kindle2jpeg.git ~/projects/kindle2jpeg
+git clone git@github.com:hellomyzn/read_later.git  ~/projects/read_later
